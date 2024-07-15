c8880ec840995d5d9164cae45b0bca5d7f3976ea
machage9603 <mikemachage@gmail.com>
2024-07-15 22:31:06 +0300

    #5: install Nginx web server w/ puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Install Nginx package
+
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => installed,
+  require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!'
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+# Define Nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
